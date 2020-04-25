fbc4dcedafddb8f0c9aa557a5f038c78296b696e
stanlylife <stianhave@hotmail.com>
2020-04-25 14:04:17 +0200

    updated layout and fixed sizing

--- assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function(){
-  console.log("loaded sucsessfully");
-
-  var vidSize = $(".vidWrap video").height();
-  console.log(".vidWrap Height: " + vidSize);
-
-  $(".bannerTxt").height(vidSize-5);
-});
