d773cf610367683fb0ca01fe14a447e469bfc6ed
Stian have <stianhave@hotmail.com>
2020-05-10 18:45:31 +0200

    final commit
    added scroll functionality and resize properties, scroll is not 100% perfect. the header elements does not scroll UP  to proper element

--- TS.js
+++ TS.js
@@ -3,15 +3,36 @@ let produktHeaderMobil = document.querySelector("#HeaderProduktMobil");
 let HeaderKontakt = document.querySelector("#HeaderKontakt");
 let HeaderFarm = document.querySelector("#HeaderFarm");
 
+let produkt = document.querySelector("#produkt");
+let produktMobil = document.querySelector("#produkt-mobil");
+let farm = document.querySelector("#farm");
+let kontakt = document.querySelector("#kontakt");
+
 (function OnLoad() {
-  console.log(produkt);
-  if (window.innerWidth < 901) {
-    produktHeader.style.display = "none";
-  } else {
-    produktHeaderMobil.style.display = "none";
-  }
+  resizeWindow();
 })();
 
-produktHeader.addEventListener((x) => {
-  console.log(x);
+function resizeWindow() {
+  if (window.innerWidth < 901) {
+    produktHeader.style.display = "none";
+    produktHeaderMobil.style.display = "flex";
+  } else {
+    produktHeader.style.display = "flex";
+    produktHeaderMobil.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", resizeWindow);
+
+produktHeader.addEventListener("click", (e) => {
+  produkt.scrollIntoView();
+});
+produktHeaderMobil.addEventListener("click", (e) => {
+  produktMobil.scrollIntoView();
+});
+HeaderKontakt.addEventListener("click", (e) => {
+  kontakt.scrollIntoView();
+});
+HeaderFarm.addEventListener("click", (e) => {
+  farm.scrollIntoView();
 });
